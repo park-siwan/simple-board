55b0e31b8d50be1e038cb3c8e98216c4ea38a632
park-siwan <siwandevelop@gmail.com>
2023-12-13 21:23:15 +0900

    feat: TypeORM Entity 작성

---
+++ src/entity/board.entity.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+
+  @ApiProperty({ description: 'user_id' })
+  @Column()
+  userId: number;
+
+  @ApiProperty({ description: '내용' })
+  @Column()
+  contents: string;
+
+  @ApiProperty({ description: '수정일' })
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @ApiProperty({ description: '생성일' })
+  @CreateDateColumn()
+  createAt: Date;
+}
