a2cbadce6fc0976605b42080b44138238c50b4d1
park-siwan <siwandevelop@gmail.com>
2023-12-15 16:45:40 +0900

    TypeORM seeding

---
+++ src/database/seeder/user.seeder.ts
@@ -0,0 +1,20 @@
+import { User } from 'src/entity/user.entity';
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+export default class UserSeeder implements Seeder {
+  track?: boolean;
+  async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager,
+  ): Promise<any> {
+    //factoryManager에 대한 규칙을 설정해서 Fake Data 생성하는 것도 가능하다.
+    const repository = dataSource.getRepository(User); //데이터 매퍼 패턴
+    await repository.insert([
+      {
+        username: 'fastcampus',
+        name: 'hong gildong',
+        passwrod: 'fastcampus1234',
+      },
+    ]);
+  }
+}
