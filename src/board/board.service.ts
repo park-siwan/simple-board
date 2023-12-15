9dde49e7db79d2d9740df32f120358c8e326f7f0
park-siwan <siwandevelop@gmail.com>
2023-12-15 17:58:34 +0900

    TypeORM 기반으로 게시글 가져오기

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
+import { Board } from 'src/entity/board.entity';
 
 @Injectable()
 export class BoardService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Board) private boardRepository: Repository<Board>,
+  ) {}
   private boards = [
     {
       id: 1,
@@ -21,12 +29,19 @@ export class BoardService {
       contents: 'Content 3',
     },
   ];
-  findAll() {
-    return this.boards;
+
+  async findAll() {
+    return await this.boardRepository.find();
   }
 
-  find(id: number) {
-    return this.boards.find((v) => v.id === id);
+  async find(id: number) {
+    const board = await this.boardRepository.findOne({
+      where: { id },
+      relations: { user: true },
+    });
+
+    if (!board) throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
+    return board;
   }
 
   create(data: CreateBoardDto) {
