a8b91c1b7ffe699a0b95c3282ebf6176bc714bdb
park-siwan <siwandevelop@gmail.com>
2023-12-18 00:46:32 +0900

    게시판에 회원 정보 적용하기

--- src/routes/board/board.controller.ts
+++ src/routes/board/board.controller.ts
@@ -7,12 +7,16 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UnauthorizedException,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { ApiTags } from '@nestjs/swagger';
-import { CreateBoardDto } from './dto/create-board.dto';
+
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserInfo } from 'src/decorators/user-info.decorator';
 
 @Controller('board')
 @ApiTags('Board')
@@ -29,8 +33,14 @@ export class BoardController {
   }
 
   @Post()
-  create(@Body(new ValidationPipe()) data: CreateBoardDto) {
-    return this.boardService.create(data);
+  @UseGuards(JwtAuthGuard)
+  create(@UserInfo() userInfo, @Body('contents') contents: string) {
+    if (!userInfo) throw new UnauthorizedException();
+
+    return this.boardService.create({
+      userId: userInfo.id,
+      contents,
+    });
   }
 
   @Put(':id')
