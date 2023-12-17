58dee015f7e7169cd3ac3af2cbf0427b2fdf2f0e
park-siwan <siwandevelop@gmail.com>
2023-12-17 10:48:20 +0900

    chore

--- src/routes/user/user.controller.ts
+++ src/routes/user/user.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
-
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
